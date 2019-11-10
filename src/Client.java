0277c6157b1b7d775a39631870e592855a09895b
Joe Walbran <walbr037@morris.umn.edu>
2019-11-10 12:44:59 -0600

    Finished configuring IntelliJ project
    Added:
     - The `src` folder
     - The `Client` class, with a `main` method. (Still a stub.)
     - An IntelliJ configuration, so that we can run the project
    
    Modified:
     - `2101-final-project.iml`, so that it recognizes `src` as the source
       folder.
     - `misc.xml` so that it uses JDK 8.

---
+++ src/Client.java
@@ -0,0 +1,11 @@
+/**
+ * The main class of this application.
+ */
+public class Client
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome to Solitaire!");
+    // TODO
+  }
+}
