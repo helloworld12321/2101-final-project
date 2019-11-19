1ccf7c534a3b5c256c715a6230afef3d5b2e7b96
Joe Walbran <walbr037@morris.umn.edu>
2019-11-19 14:40:50 -0600

    Refactored. Also, finished writing `askForMove()`.

---
+++ src/GamePrinter.java
@@ -0,0 +1,239 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * This class is in charge of printing out the state of the game.
+ *
+ * <p>
+ *   It isn't meant to be instantiated; it just provides some static methods.
+ * </p>
+ */
+class GamePrinter
+{
+  /**
+   * A mapping between the numbers of the foundation piles (0 through 3,
+   * inclusive) and the suit that corresponds to those piles.
+   */
+  private static final Map<Integer, Suit> suitsOfFoundations;
+  
+  static {
+    suitsOfFoundations = new HashMap<>();
+    suitsOfFoundations.put(0, Suit.CLUBS);
+    suitsOfFoundations.put(1, Suit.DIAMONDS);
+    suitsOfFoundations.put(2, Suit.SPADES);
+    suitsOfFoundations.put(3, Suit.HEARTS);
+  }
+  
+  /**
+   * Returns a string showing the entire solitaire game--all the cards
+   * in all the piles--as it will be displayed to the user.
+   *
+   * <p>
+   *   For more information on Solitaire terminology, see
+   *   https://cardgames.io/solitaire/#rules
+   * </p>
+   *
+   * @param foundations A {@code List} of the foundations (of which there
+   *   should be exactly 4.) This method will not mutate the foundations.
+   *
+   * @param tableaus A {@code List} of the tableaus (of which there
+   *   should be exactly 7.) This method will not mutate the tableaus.
+   *
+   * @param stock The stock of the solitaire game (that is, the pile
+   *   of face-down cards the user draws from).
+   *
+   * @param waste The waste of the solitaire game (that is, the cards that the
+   *   user has drawn from the stock.)
+   */
+  static String stringOfEverything(
+      List<Stack<Card>> foundations,
+      List<Stack<Card>> tableaus,
+      Queue<Card> stock,
+      Deque<Card> waste)
+  {
+    StringBuilder everything = new StringBuilder();
+    
+    // Print out the foundations in their own box on the top.
+    
+    String thirteenLeadingSpaces = "             ";
+    
+    String foundationsString =
+        thirteenLeadingSpaces + "  C     D     S     H  \n"
+        + thirteenLeadingSpaces + stringOfFoundations(foundations) + "\n";
+    
+    everything.append("═══════════════════════════════════════════════════\n");
+    
+    everything.append(foundationsString);
+    
+    everything.append("══════╦════════════════════════════════════════════\n");
+    
+    // Put the stock and waste on the left, and the tableaus on the right.
+    
+    String stringOfStock =
+        stock.isEmpty() ?
+            " --- " :
+            String.format("[%3s]", stock.element());
+    
+    String stringOfWaste =
+        waste.isEmpty() ?
+            " --- " :
+            String.format("[%3s]", waste.element());
+    
+    String[] leftLines = {
+        "  8  ",
+        String.format("%5s", stringOfStock),
+        "  9  ",
+        String.format("%5s", stringOfWaste),
+        };
+    
+    String tableausString = stringOfTableaus(tableaus);
+    
+    String[] rightLines =
+        ("  1     2     3     4     5     6     7  \n"
+         + tableausString).split("\\n");
+    
+    // Pad the lines on the left and the lines on the right, so that
+    // there are the same number of lines in each.
+    
+    if (leftLines.length < rightLines.length)
+    {
+      String[] newLeftLines = Arrays.copyOf(leftLines, rightLines.length);
+      for (int i = leftLines.length; i < rightLines.length; i++)
+      {
+        // five spaces.
+        newLeftLines[i] = "     ";
+      }
+      leftLines = newLeftLines;
+    }
+    else
+    {
+      String[] newRightLines = Arrays.copyOf(rightLines, leftLines.length);
+      for (int i = rightLines.length; i < leftLines.length; i++)
+      {
+        // forty-one spaces.
+        newRightLines[i] = "                                         ";
+      }
+      rightLines = newRightLines;
+    }
+    
+    for (int i = 0; i < leftLines.length; i++)
+    {
+      everything
+          .append(leftLines[i])
+          .append(" ║ ")
+          .append(rightLines[i])
+          .append("\n");
+    }
+    
+    return everything.toString();
+  }
+  
+  /**
+   * Returns a String showing all the foundation piles (as they will be
+   * displayed to the user).
+   *
+   * <p>
+   *   Only the top card of the foundation piles will be shown, so
+   *   the returned string will only have one line. This line should
+   *   have exactly 23 characters.
+   * </p>
+   *
+   * @param foundations A {@code List} of the foundations (of which there
+   *   should be exactly 4.) This method will not mutate the foundations.
+   *
+   * @return A one-line, 23-character string, showing the foundation piles in
+   *   a user-readable format. Only the top card of each pile will be shown.
+   *   If the pile has no cards in it, the symbol for the pile's suit will be
+   *   shown.
+   */
+  private static String stringOfFoundations(List<Stack<Card>> foundations)
+  {
+    List<String> cardStrings = new ArrayList<>();
+    for (int i = 0; i < foundations.size(); i++)
+    {
+      Stack<Card> foundation = foundations.get(i);
+      if (foundation.empty())
+      {
+        cardStrings.add(String.format(" -%1s- ", suitsOfFoundations.get(i)));
+      }
+      else
+      {
+        Card topCard = foundation.peek();
+        cardStrings.add(String.format("[%3s]", topCard));
+      }
+    }
+    return String.join(" ", cardStrings);
+  }
+  
+  /**
+   * Returns a String showing all the tableau piles (as they will be
+   * displayed to the user).
+   *
+   * <p>
+   *   The returned string should consist of several lines, each containing
+   *   exactly 41 characters.
+   * </p>
+   *
+   * <p>
+   *   The top of each tableau pile will be aligned, so that the cards
+   *   will look something like this:
+   * </p>
+   *
+   * <pre>
+   *   CCC CC
+   *    CC CC
+   *    C   C
+   *    C
+   * </pre>
+   *
+   *
+   * @param tableaus A {@code List} of the tableaus (of which there
+   *   should be exactly 7.) This method will not mutate the tableaus.
+   *
+   * @return A several-line string, showing the tableau piles in
+   *   a user-readable format. All of the cards in the tableau piles will
+   *   be shown, not just the top card. (If a tableau pile is empty, a suitable
+   *   "empty pile" representation will be shown instead.)
+   */
+  private static String stringOfTableaus(List<Stack<Card>> tableaus)
+  {
+    // (IntelliJ would prefer that I write "tableaux", but French is hard so
+    // I'm pluralizing it the Englishy way instead.)
+    
+    // Clone all of the tableaus before reversing them
+    @SuppressWarnings("unchecked")
+    List<Stack<Card>> reversedTableaus =
+        tableaus.stream()
+            .map(Stack<Card>::clone)
+            // .clone() will only ever return Stack<Card>.
+            // This cast is only here to satisfy the compiler. (It doesn't need
+            // to be safe at runtime; it should never fail.)
+            .map(object -> (Stack<Card>)object)
+            .collect(Collectors.toList());
+    
+    // We'll want to iterate through the tableaus from the back to the
+    // front. (So that the top items get displayed last.)
+    reversedTableaus.forEach(Collections::reverse);
+    
+    List<String> linesOfOutput = new ArrayList<>();
+    while (!reversedTableaus.stream().allMatch(Stack::empty))
+    {
+      List<String> cardStrings = new ArrayList<>();
+      for (Stack<Card> tableau : reversedTableaus)
+      {
+        if (tableau.empty())
+        {
+          // Five spaces
+          cardStrings.add("     ");
+        }
+        else
+        {
+          Card topCard = tableau.pop();
+          cardStrings.add(String.format("[%3s]", topCard));
+        }
+      }
+      linesOfOutput.add(String.join(" ", cardStrings));
+    }
+    return String.join("\n", linesOfOutput);
+  }
+}
---
+++ src/InputGetter.java
@@ -0,0 +1,185 @@
+import java.util.*;
+
+/**
+ * This class is in charge of getting input from the user.
+ *
+ * <p>
+ *   It isn't meant to be instantiated; it just provides some static methods.
+ * </p>
+ */
+class InputGetter
+{
+  /**
+   * Ask the user for a move over the standard input.
+   *
+   * <p>
+   *   This method doesn't validate that the move is legal. It does, however,
+   *   make sure that the move is from a real, existing pile to a real,
+   *   existing pile.
+   * </p>
+   *
+   * <p>
+   *   For example, if the user types <pre>asdfkljasdlf</pre> for one of the
+   *   piles, this method will prompt them to enter something better.
+   * </p>
+   *
+   * <p>
+   *   This method prints a small, minimal prompt. If you want to give
+   *   the user a more detailed message, you should print that yourself
+   *   before calling {@code askForMove()}.
+   * </p>
+   *
+   * @return The move that the user provided.
+   */
+  static Move askForMove()
+  {
+    PileTypeAndID start = askForPile("Starting pile: ");
+    PileTypeAndID destination = askForPile("Destination pile:");
+
+    return new Move(
+        start.getPileType(),
+        start.getPileID(),
+        destination.getPileType(),
+        destination.getPileID());
+  }
+  
+  /**
+   * Asks the user to input one pile over the standard input.
+   *
+   * @param prompt The string to prompt the user with. (No newline will be
+   *   printed after the prompt.)
+   *
+   * @return the pile type and ID of the pile the user entered.
+   */
+  private static PileTypeAndID askForPile(String prompt)
+  {
+    Scanner sc = new Scanner(System.in);
+
+    // Repeat until they give us a good input.
+    Move.PileType pileType = null;
+    Integer pileID = null;
+    //noinspection ConstantConditions
+    do
+    {
+      // Prompt the user.
+      System.out.print(prompt);
+      String input = sc.nextLine().trim();
+    
+      if (input.length() != 1)
+      {
+        System.out.println("Please enter a single letter or number.");
+        continue;
+      }
+    
+      if (input.matches("\\d"))
+      {
+        int intInput = Integer.parseInt(input);
+        if (1 <= intInput && intInput <= 7)
+        {
+          pileType = Move.PileType.TABLEAU;
+          pileID = intInput - 1;
+        }
+        else if (intInput == 8)
+        {
+          pileType = Move.PileType.STOCK;
+          pileID = 0;
+        }
+        else if (intInput == 9)
+        {
+          pileType = Move.PileType.WASTE;
+          pileID = 0;
+        }
+        else
+        {
+          System.out.printf(
+              "%d isn't a valid pile; please try again.\n",
+              intInput);
+        }
+      }
+      else
+      {
+        switch (input.toUpperCase())
+        {
+          case "C":
+            pileType = Move.PileType.FOUNDATION;
+            pileID = 0;
+            break;
+        
+          case "D":
+            pileType = Move.PileType.FOUNDATION;
+            pileID = 1;
+            break;
+        
+          case "S":
+            pileType = Move.PileType.FOUNDATION;
+            pileID = 2;
+            break;
+        
+          case "H":
+            pileType = Move.PileType.FOUNDATION;
+            pileID = 3;
+            break;
+        
+          default:
+            System.out.printf(
+                "%s isn't a valid pile; please try again.\n",
+                input);
+        }
+      }
+    } while (pileType == null || pileID == null);
+    
+    return new PileTypeAndID(pileType, pileID);
+  }
+
+  /**
+   * This class stores the type and ID of one pile.
+   *
+   * <p>
+   *   This class doesn't do any validation; it just stores the data it's
+   *   given.
+   * </p>
+   *
+   * <p>
+   *   (For more information on pile types and pile IDs, see the
+   *   javadoc for {@link Move}.)
+   * </p>
+   */
+  private static class PileTypeAndID
+  {
+    private Move.PileType pileType;
+    
+    private int pileID;
+  
+    /**
+     * Create a new object storing a pile type and a pile ID.
+     *
+     * @param pileType The pile type to store.
+     * @param pileID The pile ID to store.
+     */
+    private PileTypeAndID(Move.PileType pileType, int pileID)
+    {
+      this.pileType = pileType;
+      this.pileID = pileID;
+    }
+  
+    /**
+     * Return the pile type of this pile.
+     *
+     * @return The pile type of this pile.
+     */
+    Move.PileType getPileType()
+    {
+      return pileType;
+    }
+  
+    /**
+     * Return the pile ID of this pile.
+     *
+     * @return The pile ID of this pile.
+     */
+    int getPileID()
+    {
+      return pileID;
+    }
+  }
+}
