ef3e9a34868e786eb16eb11ad7df5ecd7b9b3113
Joe Walbran <walbr037@morris.umn.edu>
2019-11-19 13:30:31 -0600

    Implemented the `Move` class (untested).

--- src/Move.java
+++ src/Move.java
@@ -10,6 +10,8 @@
  *   Each move object contains information about two piles of cards: the
  *   starting pile and the destination pile. Piles are identified using two
  *   pieces of information: the <em>pile type</em> and <em>pile ID</em>.
+ *   (This information should suffice to identify every possible move,
+ *   without ambiguity.)
  * </p>
  *
  * <p>
@@ -37,26 +39,94 @@
  *   </li>
  * </ul>
  */
-public class Move
+class Move
 {
+  private final PileType startType;
+  private final int startID;
+  private final PileType destinationType;
+  private final int destinationID;
+  
+  /**
+   * Construct a new {@code Move} object.
+   *
+   * @param startType The type of pile that we're moving cards from.
+   *
+   * @param startID The ID number of the pile that we're moving cards from.
+   *
+   * @param destinationType The type of pile that we're moving cards to.
+   *
+   * @param destinationID The number of the pile that we're moving cards to.
+   */
+  Move(
+      PileType startType,
+      int startID,
+      PileType destinationType,
+      int destinationID)
+  {
+    this.startType = startType;
+    this.startID = startID;
+    this.destinationType = destinationType;
+    this.destinationID = destinationID;
+  }
+  
+  /**
+   * Returns the type of pile that we're moving cards from.
+   *
+   * @return The type of pile that we're moving cards from.
+   */
   PileType getStartType()
   {
-    return null;
+    return startType;
   }
   
+  /**
+   * Returns a number that identifies the start pile.
+   *
+   * <p>
+   *   The ID number will be unique among piles of the same type. However,
+   *   piles with different types might share the same ID number.
+   * </p>
+   *
+   * <p>
+   *   For a listing of all ID numbers, see the javadoc for the {@link Move}
+   *   class.
+   * </p>
+   *
+   * @return The ID number of the pile that we're moving cards from.
+   */
   int getStartID()
   {
-    return 0;
+    return startID;
   }
   
+  /**
+   * Returns the type of pile that we're moving cards to.
+   *
+   * @return The type of pile that we're moving cards to.
+   */
   PileType getDestinationType()
   {
-    return null;
+    return destinationType;
   }
-
+  
+  /**
+   * Returns a number that identifies the destination pile.
+   *
+   * <p>
+   *   The ID number will be unique among piles of the same type. However,
+   *   piles with different types might share the same ID number.
+   * </p>
+   *
+   * <p>
+   *   For a listing of all ID numbers, see the javadoc for the {@link Move}
+   *   class.
+   * </p>
+   *
+   * @return The ID number of the pile that we're moving cards from.
+   */
   int getDestinationID()
   {
-    return 0;
+    return destinationID;
   }
   
   enum PileType
