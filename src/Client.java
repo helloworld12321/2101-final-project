5d8914427f45c6723b08d861863ddbeab39bdeba
Joe Walbran <walbr037@morris.umn.edu>
2019-12-01 11:52:49 -0600

    Refactored the client

--- src/Client.java
+++ src/Client.java
@@ -15,72 +15,95 @@ public class Client
   public static void main(String[] args)
   {
     System.out.println("Welcome to Solitaire!");
-
     game = new SolitaireGame();
     
+    MoveResult previousMoveResult = MoveResult.LEGAL;
     // TODO: Add check for is-the-game-finished.
-    while (true)
+    while (previousMoveResult != MoveResult.QUIT)
     {
-      printGameState();
+      previousMoveResult = makeMove(previousMoveResult);
+    }
+    System.out.println("Thanks for playing!");
+  }
+  
+  /**
+   * These enum values represent the various things that could happen
+   * when the user makes a move.
+   */
+  private enum MoveResult
+  {
+    /**
+     * The move was legal, and the game state was changed accordingly.
+     */
+    LEGAL,
+  
+    /**
+     * The move was illegal, so the game state was not changed.
+     */
+    ILLEGAL,
+  
+    /**
+     * The user wants to quit.
+     */
+    QUIT
+  }
+  
+  /**
+   * Ask the user for a move, and try to execute it.
+   *
+   * @param previousMoveResult whether the last move was legal or not.
+   *
+   * @return whether the move was made successfully.
+   */
+  private static MoveResult makeMove(MoveResult previousMoveResult)
+  {
+    if (previousMoveResult == MoveResult.LEGAL)
+    {
+      System.out.println(stringOfGame());
+      System.out.println();
       System.out.println("Please enter a move.");
-      
-      // Keep prompting for user input until they enter something legit.
-      while (true)
-      {
-        Move move;
-        try
-        {
-          move = InputGetter.askForMove();
-        }
-        catch (QuitTheGameException e)
-        {
-          quit();
-          
-          // These lines should never be reached.
-          assert false;
-          return;
-        }
-
-        try
-        {
-          game.makeMove(move);
-
-          // If we reach this line, makeMove didn't throw,
-          // and the move the user entered was legal.
-          break;
-        }
-        catch (IllegalMoveException e)
-        {
-          System.out.printf("\tThat move isn't legal: %s\n", e.getMessage());
-          System.out.println("Please enter another move.");
-          continue;
-        }
-      }
+    }
+    else
+    {
+      System.out.println("Please enter another move.");
+    }
+    
+    Move move;
+    try
+    {
+      move = InputGetter.askForMove();
+    }
+    catch (QuitTheGameException e)
+    {
+      return MoveResult.QUIT;
+    }
+  
+    try
+    {
+      game.makeMove(move);
+    
+      // If we reach this line, makeMove didn't throw,
+      // and the move the user entered was legal.
+      return MoveResult.LEGAL;
+    }
+    catch (IllegalMoveException e)
+    {
+      System.out.printf("\tThat move isn't legal: %s\n", e.getMessage());
+      return MoveResult.ILLEGAL;
     }
   }
   
   /**
-   * Quit the game and terminate the program.
+   * Returns a string representing the state of the game.
    *
    * <p>
-   *   The program will terminate with exit code {@code 0}.
+   *   The returned string shows all of the cards on the table in a pretty,
+   *   human-readable format.
    * </p>
-   */
-  static void quit()
-  {
-    System.out.println("Thanks for playing!");
-    System.exit(0);
-  }
-
-  /**
-   * Display the state of the game on the standard output.
    *
-   * <p>
-   *   This method prints all of the cards on the table in a pretty, human-
-   *   readable format.
-   * </p>
+   * @return a string representing the state of the game.
    */
-  private static void printGameState()
+  private static String stringOfGame()
   {
     List<Stack<Card>> foundations = new ArrayList<>();
     for (int i = 0; i < 4; i++)
@@ -98,10 +121,10 @@ public class Client
   
     Deque<Card> waste = game.getWaste();
   
-    System.out.println(GamePrinter.stringOfEverything(
+    return GamePrinter.stringOfEverything(
         foundations,
         tableaus,
         stock,
-        waste));
+        waste);
   }
 }
